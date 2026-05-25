494c58fe9f46efdb1d82fe583b84d4fb98605912
David Danso Nyame <davnyame1233@gmail.com>
2026-05-25 18:09:34 +0000

    Fix typo in monthStr list for February

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ day:list[str] = [
     'Friday', 'Saturday', 'Sunday'
 ]
 monthStr:list[str] = [
-    "January", "Febuary", "March",
+    "January", "February", "March",
     "April", "May", "June",
     "July", "August", "September",
     "October", "November", "December"
